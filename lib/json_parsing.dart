7b45b48cc24958d90b3ecbcbac6f56573bcc1c47
Emilson Silva <emilson.silva@tgcore.com.br>
2020-06-04 12:04:13 -0300

    create jason_parsing.dart, jason_test, e factory article,

--- lib/json_parsing.dart
+++ lib/json_parsing.dart
@@ -1,25 +1,31 @@
+import 'dart:convert' as json;
+
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+import 'package:hnapp/serializers.dart';
 
 part 'json_parsing.g.dart';
 
 abstract class Article implements Built<Article, ArticleBuilder> {
+  static Serializer<Article> get serializer => _$articleSerializer;
+
   int get id;
 
   bool get deleted;
-  String get type;	 //"job", "story", "comment", "poll", or "pollopt".
+  String get type; //"job", "story", "comment", "poll", or "pollopt".
   String get by;
   int get time;
   String get text;
   bool get dead;
   int get parent;
   int get poll;
-  BuiltList<int> kids;
-  url
-  score
-  title
-  parts
-  descendants
+  BuiltList<int> get kids;
+  String get url;
+  int get score;
+  String get title;
+  BuiltList<int> get parts;
+  int get descendants;
 
   Article._();
   factory Article([updates(ArticleBuilder b)]) = _$Article;
@@ -33,8 +39,7 @@ List<int> parseTopStories(String jsonStr) {
 }
 
 Article parseArticle(String jsonStr) {
-  return null;
-  /* final parsed = json.jsonDecode(jsonStr);
-  Article article = Article.fromJson(parsed);
-  return article;*/
+  final parsed = json.jsonDecode(jsonStr);
+  Article article = serializers.deserializeWith(Article.serializer, parsed);
+  return article;
 }
