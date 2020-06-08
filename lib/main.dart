7e3cbb824469de664541b6c4d0502dae79bb0615
Emilson Silva <emilson.silva@tgcore.com.br>
2020-06-08 15:44:03 -0300

    create jason_parsing.dart, jason_test, e factory article,

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'dart:collection';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hnapp/src/article.dart';
+import 'package:hnapp/src/hn_bloc.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(MyApp());
+  final hnBloc = HackerNewsBloc();
+  runApp(MyApp(bloc: hnBloc));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final HackerNewsBloc bloc;
+
+  MyApp({
+    Key key,
+    this.bloc,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,13 +28,22 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'Flutter Demo Home Page',
+        bloc: bloc,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  final HackerNewsBloc bloc;
+
+  MyHomePage({
+    Key key,
+    this.title,
+    this.bloc,
+  }) : super(key: key);
 
   final String title;
 
@@ -36,27 +55,16 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView(
-        children: _ids
-            .map((i) => FutureBuilder<Article>(
-                  future: _getArticle(i),
-                  builder:
-                      (BuildContext context, AsyncSnapshot<Article> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      return _buildItem(snapshot.data);
-                    } else {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                  },
-                ))
-            .toList(),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: StreamBuilder<UnmodifiableListView<Article>>(
+          stream: widget.bloc.articles,
+          initialData: UnmodifiableListView<Article>([]),
+          builder: (context, snapshot) => ListView(
+            children: snapshot.data.map(_buildItem).toList(),
+          ),
+        ));
   }
 
   Widget _buildItem(Article article) {
