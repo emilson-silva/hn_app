ac3912926e3daf199f549eb89c552b3380bfbbd7
Emilson Silva <emilson.silva@tgcore.com.br>
2020-06-08 14:10:47 -0300

    create jason_parsing.dart, jason_test, e factory article,

--- lib/main.dart
+++ lib/main.dart
@@ -34,48 +34,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<int> _ids = [
-    23430282,
-    23430332,
-    23428398,
-    23431150,
-    23430671,
-    23425041,
-    23422475,
-    23425508,
-    23426189,
-    23430880,
-    23425524,
-    23429855,
-    23426538,
-    23430142,
-    23414793,
-    23415668,
-    23429273,
-    23430861,
-    23426752,
-    23429959,
-    23427689,
-    23414872,
-    23430575,
-    23415034,
-    23428547,
-    23427186,
-    23428432,
-    23415175,
-    23426154,
-    23415682,
-    23418699,
-  ];
-
-  Future<Article> _getArticle(int id) async {
-    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
-    final storyRes = await http.get(storyUrl);
-    if (storyRes.statusCode == 200) {
-      return parseArticle(storyRes.body);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/src/hn_bloc.dart
+++ lib/src/hn_bloc.dart
@@ -1,7 +1,59 @@
 import 'package:hnapp/src/article.dart';
+import 'package:http/http.dart' as http;
+import 'package:rxdart/rxdart.dart';
 
 class HackerNewsBloc {
   Stream<List<Article>> get articles => _articlesSubject.stream;
 
   final _articlesSubject = BehaviorSubject<List<Article>>();
+  var _articles = <Article>[];
+
+  HackerNewsBloc() {}
+  List<int> _ids = [
+    23430282,
+    23430332,
+    23428398,
+    23431150,
+    23430671,
+    23425041,
+    23422475,
+    23425508,
+    23426189,
+    23430880,
+    23425524,
+    23429855,
+    23426538,
+    23430142,
+    23414793,
+    23415668,
+    23429273,
+    23430861,
+    23426752,
+    23429959,
+    23427689,
+    23414872,
+    23430575,
+    23415034,
+    23428547,
+    23427186,
+    23428432,
+    23415175,
+    23426154,
+    23415682,
+    23418699,
+  ];
+
+  Future<List<Article>> _getArticles() async {
+    final futureArticles = _ids.map((id) => _getArticle(id));
+    final articles = await Future.wait(futureArticles);
+    return articles;
+  }
+
+  Future<Article> _getArticle(int id) async {
+    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
+    final storyRes = await http.get(storyUrl);
+    if (storyRes.statusCode == 200) {
+      return parseArticle(storyRes.body);
+    }
+  }
 }
