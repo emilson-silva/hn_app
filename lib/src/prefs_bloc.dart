ea4c66fc2daa53054301e5b77dcbcd486edda527
Emilson Silva <emilson.silva@tgcore.com.br>
2020-06-25 16:19:32 -0300

    alterações

--- lib/src/prefs_bloc.dart
+++ lib/src/prefs_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:rxdart/rxdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PrefsBlocError extends Error {
   final String message;
@@ -15,13 +16,16 @@ class PrefsState {
 }
 
 class PrefsBloc {
-  final _currentPrefs = BehaviorSubject<PrefsState>(
-    seedValue: PrefsState(true),
-  );
+  final _currentPrefs = BehaviorSubject<PrefsState>.seeded(PrefsState(true));
 
   final _showWebViewPref = StreamController<bool>();
 
-  PrefsBloc() {}
+  PrefsBloc() {
+    _loadSharedPrefs();
+    _showWebViewPref.stream.listen((bool) {
+      _saveNewPrefs(PrefsState(bool));
+    });
+  }
 
   Stream<PrefsState> get currentPrefs => _currentPrefs.stream;
 
@@ -30,5 +34,17 @@ class PrefsBloc {
   void close() {
     _showWebViewPref.close();
     _currentPrefs.close();
+  }
+
+  Future<void> _loadSharedPrefs() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    final showWebView = sharedPrefs.getBool('showWebView') ?? true;
+    _currentPrefs.add(PrefsState(showWebView));
+  }
+
+  Future<void> _saveNewPrefs(PrefsState newState) async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    await sharedPrefs.setBool('showWebView', newState.showWebView);
+    _currentPrefs.add(newState);
   }
 }
